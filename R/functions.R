9c135aeb1cd7d2d48de715f577203f4997dcdac5
Tim Hister <tim.hister@gov.bc.ca>
2025-11-27 13:36:38 -0800

    fixed ggplot colors

--- R/functions.R
+++ R/functions.R
@@ -1,12 +1,11 @@
-pacman::p_load(tidyverse, echarts4r, bslib, shinyWidgets)
+# NOTES TO SELF:
 
-# setup_fn = function() {
-#   invisible(extrafont::choose_font("BC Sans", quiet = T))
-#   extrafont::loadfonts()
-#
-#   theme_set(ggthemes::theme_clean(base_size = 14, base_family = "BC Sans"))
-#   e_common(font_family = "BC Sans", theme = "Westeros")
-# }
+# - I want FTE, earnings, etc., by month (or week or day?) rather than year
+# - can we find out which FPs are on LFP and which aren't?
+# - do FPs switch out of hospitalism after LFP?
+
+
+pacman::p_load(tidyverse, echarts4r, bslib, shinyWidgets)
 
 ggpad = function(m=.1) scale_x_continuous(expand = expansion(mult = m))
 
@@ -23,287 +22,345 @@ get_cihi_data = function(file = Sys.getenv("CIHI_PATH")) {
     mutate(across(c(jurisdiction, health_region, specialty), fct))
 }
 
-prep_plots1 = function(data) {
-  default_provinces = c("B.C.", "Alta.", "Ont.", "Man.")
-
+prep_cihi_data_1 = function(data) {
   df = data |>
     filter(
       specialty == "All physicians",
       as.character(jurisdiction) == as.character(health_region),
       jurisdiction != 'Canada'
     )
+}
 
-  all_provinces = unique(df$jurisdiction)
-
-  ui = bslib::page_fluid(
+make_ui_1 = function(data) {
+  all_provinces = unique(data$jurisdiction)
+  bslib::page_fluid(
     virtualSelectInput("prov", "Prov?", choices = all_provinces, multiple = T, selected = "B.C."),
     card(plotOutput("p1"), full_screen = T)
   )
+}
 
-  g = function(data) data |>
+# don't need this guy as a target
+g_1_fn = function(data) {
+  all_provinces = unique(data$jurisdiction)
+  prov_colors = viridis::magma(length(all_provinces))
+  names(prov_colors) = all_provinces
+  prov_colors[["B.C."]] = HA_colors()[[1]]
+
+  data |>
     ggplot() +
     aes(x=year, y=number_of_physicians, color=jurisdiction) +
-    scale_color_viridis_d() +
     scale_y_continuous(labels = scales::label_comma()) +
     ggtitle("Physicians") +
     ggpad() +
-    geom_line() +
-    geom_point()
+    geom_line(aes(linetype = jurisdiction != "B.C.")) +
+    geom_point(aes(size = if_else(jurisdiction == 'B.C.', 2, 1))) +
+    scale_color_manual(values = prov_colors) +
+    scale_size_identity() +
+    guides(size = 'none', linetype = 'none')
+}
 
-  g1 = g(filter(df, jurisdiction %in% default_provinces))
 
-  e1 = filter(df, jurisdiction %in% default_provinces) |>
+plot_g_1 = function(data) {
+  default_provinces = c("B.C.", "Alta.", "Ont.", "Man.")
+  data |>
+    filter(jurisdiction %in% default_provinces) |>
+    g_1_fn()
+}
+
+plot_e_1 = function(data) {
+  default_provinces = c("B.C.", "Alta.", "Ont.", "Man.")
+  data |>
+    filter(jurisdiction %in% default_provinces) |>
     mutate(year = fct(as.character(year))) |>
     group_by(jurisdiction) |>
     e_charts(x = year) |>
     e_line(number_of_physicians) |>
     e_title("# Physicians") |>
     e_tooltip('axis')
-
-  list(df=df, ui=ui, g=g, g1=g1, e1=e1)
 }
 
-plot1 = function(data, type) {
-  if (type == 'g') {
-    ggplot() +
-      scale_color_viridis_d() +
-      scale_y_continuous(labels = scales::label_comma()) +
-      ggtitle("Physicians in B.C.") +
-      ggpad()
-  } else {
-    df |>
-      mutate(year = fct(as.character(year))) |>
-      e_charts(x = year) |>
-      e_line(number_of_physicians) |>
-      e_title("# Physicians", "BC") |>
-      e_tooltip('axis') |>
-      e_y_axis(number_of_physicians)
-  }
-}
 
-print_server_2 = function(data) {
-  print(data)
-  data |>
-    mutate(is_bc = health_region == 'B.C.') |>
-    mutate(is_bc_scale = case_when(is_bc ~ 1.5, T ~ 1)) |>
-    ggplot(aes(x=year, y=phys_pop_ratio, color=jurisdiction, linewidth=is_bc_scale)) +
-    geom_line() +
-    geom_point() +
-    scale_color_viridis_d() +
-    scale_y_continuous(labels = scales::label_comma()) +
-    guides(linewidth = 'none', size = 'none') +
-    ggtitle("Physicians per Pop - across Canada") +
-    theme(legend.position = 'bottom') +
-    ggpad()
-}
+# prep_plots1 = function(data) {
+#   default_provinces = c("B.C.", "Alta.", "Ont.", "Man.")
 
-print_ui5 = function(data) {
-  specialties = data |>
-    select(year, jurisdiction, health_region, specialty, number_of_physicians) |>
-    filter(health_region == 'B.C.') |>
-    #filter(str_detect(specialty, "^[_|All]", negate = T))
-    pull(specialty) |>
-    unique() |>
-    sort()
+  # df = data |>
+  #   filter(
+  #     specialty == "All physicians",
+  #     as.character(jurisdiction) == as.character(health_region),
+  #     jurisdiction != 'Canada'
+  #   )
 
-  page_sidebar(
-    sidebar = sidebar(
-      virtualSelectInput("specs", "Spec?", choices = specialties, multiple = T, selected = "Family medicine")
-    ),
-    card(
-      plotOutput("p5"), full_screen = T
-    )
-  )
-}
+  # all_provinces = unique(df$jurisdiction)
 
-# print_server_5 = function(data) {
-#   data |>
-#     ggplot(aes(x=year, y=number_of_physicians, color=specialty)) +
-#     geom_point() +
-#     geom_line() +
-#     scale_y_continuous(labels = scales::label_comma())
+  # ui = bslib::page_fluid(
+  #   virtualSelectInput("prov", "Prov?", choices = all_provinces, multiple = T, selected = "B.C."),
+  #   card(plotOutput("p1"), full_screen = T)
+  # )
+
+  # g = function(data) data |>
+  #   ggplot() +
+  #   aes(x=year, y=number_of_physicians, color=jurisdiction) +
+  #   scale_color_viridis_d() +
+  #   scale_y_continuous(labels = scales::label_comma()) +
+  #   ggtitle("Physicians") +
+  #   ggpad() +
+  #   geom_line() +
+  #   geom_point()
+
+  #g1 = g(filter(df, jurisdiction %in% default_provinces))
+
+#   e1 = filter(df, jurisdiction %in% default_provinces) |>
+#     mutate(year = fct(as.character(year))) |>
+#     group_by(jurisdiction) |>
+#     e_charts(x = year) |>
+#     e_line(number_of_physicians) |>
+#     e_title("# Physicians") |>
+#     e_tooltip('axis')
+#
+#   list(df=df, ui=ui, g=g, g1=g1, e1=e1)
 # }
 
-
-
-plot2 = function(data, type) {
-  good_provinces = c("Canada", "B.C.", "Alta.", "Ont.", "Man.")
-
-  df = data |>
-    filter(
-      specialty == "All physicians",
-      health_region %in% good_provinces
-    )
-
-  if (type == 'g') {
-    df |>
-      mutate(is_bc = health_region == 'B.C.') |>
-      mutate(is_bc_scale = case_when(is_bc ~ 1.5, T ~ 1)) |>
-      ggplot(aes(x=year, y=phys_pop_ratio, color=jurisdiction, linewidth=is_bc_scale)) +
-      geom_line() +
-      geom_point() +
-      scale_color_viridis_d() +
-      scale_y_continuous(labels = scales::label_comma()) +
-      guides(linewidth = 'none', size = 'none') +
-      ggtitle("Physicians per Pop - across Canada") +
-      theme(legend.position = 'bottom') +
-      ggpad()
-  } else {
-    df |>
-      mutate(year = fct(as.character(year))) |>
-      group_by(jurisdiction) |>
-      e_charts(x = year) |>
-      e_line(serie = phys_pop_ratio) |>
-      e_title("# Physicians", "Canada/Provinces") |>
-      e_tooltip('axis') |>
-      e_theme("westeros") |>
-      e_y_axis(phys_pop_ratio, formatter = e_axis_formatter("decimal"), margin=30)
-  }
-}
-
-plot3 = function(data, type) {
-  df = data |>
-    filter(
-      jurisdiction == 'B.C.',
-      health_region == 'B.C.',
-      specialty == "All physicians"
-    )
-
-  df2 = data |>
-    filter(
-      jurisdiction == 'B.C.',
-      health_region == 'B.C.',
-      specialty == "All physicians",
-      year %in% c(2015, 2020, 2024)
-    ) |>
-    select(year, starts_with("age_group")) |>
-    rename("00-30" = age_group_younger_than_30, "80-100" = age_group_80_and_older) |>
-    select(-age_group_unknown) |>
-    pivot_longer(cols = 2:last_col()) |>
-    mutate(name = str_extract(name, "\\d\\d-\\d\\d")) |>
-    rename(age_group = name)
-
-  df3 = df2 |>
-    group_by(year) |>
-    summarise(total_pop = sum(value)) |>
-    inner_join(df2) |>
-    mutate(prop_phys = value / total_pop) |>
-    mutate(year = fct(as.character(year)))
-
-  if (type == 'g') {
-    g1 = df |>
-      ggplot(aes(x=year, y=median_age)) +
-      geom_line() +
-      geom_point() +
-      scale_color_viridis_d() +
-      theme(legend.position = 'none') +
-      scale_y_continuous(labels = scales::label_comma()) +
-      ggtitle("Median Physician Age") +
-      ggpad()
-
-    g2 = df3 |>
-      ggplot(aes(x=age_group, y=prop_phys, color=year, group=year)) +
-      geom_line() +
-      geom_point() +
-      scale_color_viridis_d() +
-      scale_y_continuous(labels = scales::label_percent()) +
-      ggtitle("Relative Age Distribution by Year") +
-      theme(legend.position = 'bottom')
-
-    g1 + g2
-  } else {
-    e1 = df |>
-      mutate(year = fct(as.character(year))) |>
-      e_chart(x=year) |>
-      e_line(median_age) |>
-      e_y_axis(median_age) |>
-      e_title("Median Physician Age", "BC") |>
-      e_tooltip('axis')
-
-    e2 = df3 |>
-      group_by(year) |>
-      e_chart(x=age_group) |>
-      e_line(prop_phys) |>
-      e_title("Relative Age Distribution by Year") |>
-      e_tooltip('axis') |>
-      e_legend(right=0) |>
-      e_y_axis(formatter = e_axis_formatter('percent'))
-
-    browsable(
-      div(
-        style = "display: flex; gap: 20px;",
-        div(style = "flex: 1;", e1),
-        div(style = "flex: 1;", e2)
-      )
-    )
-  }
-}
-
-plot4 = function(data, type) {
-  df = data |>
-    filter(
-      jurisdiction == 'B.C.',
-      health_region == 'B.C.',
-      specialty == "All physicians"
-    ) |>
-    select(year, number_of_physicians, number_female, number_male) |>
-    mutate(across(3:4, ~./number_of_physicians)) |>
-    rename_with(~str_replace_all(., "number", "prop"), 3:4) |>
-    select(year, starts_with("prop")) |>
-    pivot_longer(2:3) |>
-    mutate(name = str_remove(name, "prop_"))
-
-  if (type == 'g') {
-    df |>
-      ggplot(aes(x=year, y=value, color=name)) +
-      geom_line() +
-      geom_point() +
-      scale_color_viridis_d() +
-      theme(legend.position = 'bottom') +
-      scale_y_continuous(labels = scales::label_percent()) +
-      labs(color = NULL) +
-      ggtitle("Gender Distribution") +
-      ggpad()
-  } else {
-    df |>
-      mutate(year = fct(as.character(year))) |>
-      group_by(name) |>
-      e_chart(x=year) |>
-      e_line(value) |>
-      e_title("Gender Distribution") |>
-      e_y_axis(formatter = e_axis_formatter('percent')) |>
-      e_tooltip('axis')
-  }
-}
-
-plot5 = function(data, type) {
-  df = data |>
-    select(year, jurisdiction, health_region, specialty, number_of_physicians) |>
-    filter(health_region == 'B.C.') |>
-    filter(str_detect(specialty, "^[_|All]", negate = T))
-  if (type == 'g') {
-    df |>
-      ggplot(aes(x=year, y=number_of_physicians, color=specialty)) +
-      geom_point() +
-      geom_line() +
-      scale_y_continuous(labels = scales::label_comma())
-
-  } else {
-    df |>
-      mutate(year = fct(as.character(year))) |>
-      group_by(specialty) |>
-      e_chart(x=year) |>
-      e_line(number_of_physicians) |>
-      e_title("number_of_physicians") |>
-      e_tooltip('axis') |>
-      e_legend_scroll()
-  }
-
-}
-
-make_milestones = function() {
-  tribble(~date, ~name, ~specialty, ~other,
-    "2023-02-01", "LFP", "Family medicine", NA_character_
-) |>
-    mutate(date = ymd(date))
-}
+# plot1 = function(data, type) {
+#   if (type == 'g') {
+#     ggplot() +
+#       scale_color_viridis_d() +
+#       scale_y_continuous(labels = scales::label_comma()) +
+#       ggtitle("Physicians in B.C.") +
+#       ggpad()
+#   } else {
+#     df |>
+#       mutate(year = fct(as.character(year))) |>
+#       e_charts(x = year) |>
+#       e_line(number_of_physicians) |>
+#       e_title("# Physicians", "BC") |>
+#       e_tooltip('axis') |>
+#       e_y_axis(number_of_physicians)
+#   }
+# }
+#
+# print_server_2 = function(data) {
+#   print(data)
+#   data |>
+#     mutate(is_bc = health_region == 'B.C.') |>
+#     mutate(is_bc_scale = case_when(is_bc ~ 1.5, T ~ 1)) |>
+#     ggplot(aes(x=year, y=phys_pop_ratio, color=jurisdiction, linewidth=is_bc_scale)) +
+#     geom_line() +
+#     geom_point() +
+#     scale_color_viridis_d() +
+#     scale_y_continuous(labels = scales::label_comma()) +
+#     guides(linewidth = 'none', size = 'none') +
+#     ggtitle("Physicians per Pop - across Canada") +
+#     theme(legend.position = 'bottom') +
+#     ggpad()
+# }
+#
+# print_ui5 = function(data) {
+#   specialties = data |>
+#     select(year, jurisdiction, health_region, specialty, number_of_physicians) |>
+#     filter(health_region == 'B.C.') |>
+#     #filter(str_detect(specialty, "^[_|All]", negate = T))
+#     pull(specialty) |>
+#     unique() |>
+#     sort()
+#
+#   page_sidebar(
+#     sidebar = sidebar(
+#       virtualSelectInput("specs", "Spec?", choices = specialties, multiple = T, selected = "Family medicine")
+#     ),
+#     card(
+#       plotOutput("p5"), full_screen = T
+#     )
+#   )
+# }
+#
+# # print_server_5 = function(data) {
+# #   data |>
+# #     ggplot(aes(x=year, y=number_of_physicians, color=specialty)) +
+# #     geom_point() +
+# #     geom_line() +
+# #     scale_y_continuous(labels = scales::label_comma())
+# # }
+#
+#
+#
+# plot2 = function(data, type) {
+#   good_provinces = c("Canada", "B.C.", "Alta.", "Ont.", "Man.")
+#
+#   df = data |>
+#     filter(
+#       specialty == "All physicians",
+#       health_region %in% good_provinces
+#     )
+#
+#   if (type == 'g') {
+#     df |>
+#       mutate(is_bc = health_region == 'B.C.') |>
+#       mutate(is_bc_scale = case_when(is_bc ~ 1.5, T ~ 1)) |>
+#       ggplot(aes(x=year, y=phys_pop_ratio, color=jurisdiction, linewidth=is_bc_scale)) +
+#       geom_line() +
+#       geom_point() +
+#       scale_color_viridis_d() +
+#       scale_y_continuous(labels = scales::label_comma()) +
+#       guides(linewidth = 'none', size = 'none') +
+#       ggtitle("Physicians per Pop - across Canada") +
+#       theme(legend.position = 'bottom') +
+#       ggpad()
+#   } else {
+#     df |>
+#       mutate(year = fct(as.character(year))) |>
+#       group_by(jurisdiction) |>
+#       e_charts(x = year) |>
+#       e_line(serie = phys_pop_ratio) |>
+#       e_title("# Physicians", "Canada/Provinces") |>
+#       e_tooltip('axis') |>
+#       e_theme("westeros") |>
+#       e_y_axis(phys_pop_ratio, formatter = e_axis_formatter("decimal"), margin=30)
+#   }
+# }
+#
+# plot3 = function(data, type) {
+#   df = data |>
+#     filter(
+#       jurisdiction == 'B.C.',
+#       health_region == 'B.C.',
+#       specialty == "All physicians"
+#     )
+#
+#   df2 = data |>
+#     filter(
+#       jurisdiction == 'B.C.',
+#       health_region == 'B.C.',
+#       specialty == "All physicians",
+#       year %in% c(2015, 2020, 2024)
+#     ) |>
+#     select(year, starts_with("age_group")) |>
+#     rename("00-30" = age_group_younger_than_30, "80-100" = age_group_80_and_older) |>
+#     select(-age_group_unknown) |>
+#     pivot_longer(cols = 2:last_col()) |>
+#     mutate(name = str_extract(name, "\\d\\d-\\d\\d")) |>
+#     rename(age_group = name)
+#
+#   df3 = df2 |>
+#     group_by(year) |>
+#     summarise(total_pop = sum(value)) |>
+#     inner_join(df2) |>
+#     mutate(prop_phys = value / total_pop) |>
+#     mutate(year = fct(as.character(year)))
+#
+#   if (type == 'g') {
+#     g1 = df |>
+#       ggplot(aes(x=year, y=median_age)) +
+#       geom_line() +
+#       geom_point() +
+#       scale_color_viridis_d() +
+#       theme(legend.position = 'none') +
+#       scale_y_continuous(labels = scales::label_comma()) +
+#       ggtitle("Median Physician Age") +
+#       ggpad()
+#
+#     g2 = df3 |>
+#       ggplot(aes(x=age_group, y=prop_phys, color=year, group=year)) +
+#       geom_line() +
+#       geom_point() +
+#       scale_color_viridis_d() +
+#       scale_y_continuous(labels = scales::label_percent()) +
+#       ggtitle("Relative Age Distribution by Year") +
+#       theme(legend.position = 'bottom')
+#
+#     g1 + g2
+#   } else {
+#     e1 = df |>
+#       mutate(year = fct(as.character(year))) |>
+#       e_chart(x=year) |>
+#       e_line(median_age) |>
+#       e_y_axis(median_age) |>
+#       e_title("Median Physician Age", "BC") |>
+#       e_tooltip('axis')
+#
+#     e2 = df3 |>
+#       group_by(year) |>
+#       e_chart(x=age_group) |>
+#       e_line(prop_phys) |>
+#       e_title("Relative Age Distribution by Year") |>
+#       e_tooltip('axis') |>
+#       e_legend(right=0) |>
+#       e_y_axis(formatter = e_axis_formatter('percent'))
+#
+#     browsable(
+#       div(
+#         style = "display: flex; gap: 20px;",
+#         div(style = "flex: 1;", e1),
+#         div(style = "flex: 1;", e2)
+#       )
+#     )
+#   }
+# }
+#
+# plot4 = function(data, type) {
+#   df = data |>
+#     filter(
+#       jurisdiction == 'B.C.',
+#       health_region == 'B.C.',
+#       specialty == "All physicians"
+#     ) |>
+#     select(year, number_of_physicians, number_female, number_male) |>
+#     mutate(across(3:4, ~./number_of_physicians)) |>
+#     rename_with(~str_replace_all(., "number", "prop"), 3:4) |>
+#     select(year, starts_with("prop")) |>
+#     pivot_longer(2:3) |>
+#     mutate(name = str_remove(name, "prop_"))
+#
+#   if (type == 'g') {
+#     df |>
+#       ggplot(aes(x=year, y=value, color=name)) +
+#       geom_line() +
+#       geom_point() +
+#       scale_color_viridis_d() +
+#       theme(legend.position = 'bottom') +
+#       scale_y_continuous(labels = scales::label_percent()) +
+#       labs(color = NULL) +
+#       ggtitle("Gender Distribution") +
+#       ggpad()
+#   } else {
+#     df |>
+#       mutate(year = fct(as.character(year))) |>
+#       group_by(name) |>
+#       e_chart(x=year) |>
+#       e_line(value) |>
+#       e_title("Gender Distribution") |>
+#       e_y_axis(formatter = e_axis_formatter('percent')) |>
+#       e_tooltip('axis')
+#   }
+# }
+#
+# plot5 = function(data, type) {
+#   df = data |>
+#     select(year, jurisdiction, health_region, specialty, number_of_physicians) |>
+#     filter(health_region == 'B.C.') |>
+#     filter(str_detect(specialty, "^[_|All]", negate = T))
+#   if (type == 'g') {
+#     df |>
+#       ggplot(aes(x=year, y=number_of_physicians, color=specialty)) +
+#       geom_point() +
+#       geom_line() +
+#       scale_y_continuous(labels = scales::label_comma())
+#
+#   } else {
+#     df |>
+#       mutate(year = fct(as.character(year))) |>
+#       group_by(specialty) |>
+#       e_chart(x=year) |>
+#       e_line(number_of_physicians) |>
+#       e_title("number_of_physicians") |>
+#       e_tooltip('axis') |>
+#       e_legend_scroll()
+#   }
+#
+# }
+#
+# make_milestones = function() {
+#   tribble(~date, ~name, ~specialty, ~other,
+#     "2023-02-01", "LFP", "Family medicine", NA_character_
+# ) |>
+#     mutate(date = ymd(date))
+# }
--- _targets.R
+++ _targets.R
@@ -3,10 +3,14 @@ tar_source()
 tar_option_set(packages = c("tidyverse", "hsiaR", "patchwork", "echarts4r", "htmltools", "shiny", "bslib", "shinyWidgets"))
 
 targets = list(
-  tar_target(file, Sys.getenv("CIHI_PATH"), format = "file"),
-  tar_target(cihi, get_cihi_data(file)),
-  tar_target(milestones, make_milestones()),
-  tar_target(c1, prep_plots1(cihi))
+  tar_target(cihi_file, Sys.getenv("CIHI_PATH"), format = "file"),
+  tar_target(cihi, get_cihi_data(cihi_file)),
+  # tar_target(milestones, make_milestones()),
+  tar_target(cihi_data_1, prep_cihi_data_1(cihi)),
+  tar_target(ui_1, make_ui_1(cihi_data_1)),
+  tar_target(g_1, plot_g_1(cihi_data_1)),
+  tar_target(e_1, plot_e_1(cihi_data_1))
+  # tar_target(c1, prep_plots1(cihi))
   # tar_target(g1, plot1(cihi, 'g')),
   # tar_target(e1, plot1(cihi, 'e')),
   # tar_target(g2, plot2(cihi, 'g')),
